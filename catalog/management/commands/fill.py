07380917f1b1a2bff653f472b4f1a1bc27d3b4f3
anastasia <nastia.riabtseva18@gmail.com>
2023-08-24 20:37:57 +0300

    added pictures in the folder media/product, templates home.html, contacts.html, inc_catalog_menu.html, my_tag for displaying pictures, fixed data filling in the file and add files data_category.json, data_product.json, added static folder with css and js files, added migration 0002, add .gitignore

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,39 +1,36 @@
+import json
 from django.core.management import BaseCommand
-from django.utils import timezone
-from django.utils.datetime_safe import date
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-
-        product_list = [
-            {'title_product': 'Молоко', 'description_product': 'Milk', 'category': 'Молочные продукты', 'price': 1.87,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Говядина', 'description_product': 'Beef', 'category': 'Мясные продукты', 'price': 5.49,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Помидор', 'description_product': 'Tomato', 'category': 'Овощи', 'price': 3.99,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Огурец', 'description_product': 'Cucumber', 'category': 'Овощи', 'price': 1.59,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Яблоко', 'description_product': 'Аpple', 'category': 'Фрукты', 'price': 2.49,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Виноград', 'description_product': 'Grape', 'category': 'Фрукты', 'price': 5.99,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Батон', 'description_product': 'Baton', 'category': 'Хлебобyлочные изделия',
-             'price': 0.95,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()},
-            {'title_product': 'Йогурт', 'description_product': 'Yogurt', 'category': 'Молочные продукты', 'price': 1.25,
-             'date_of_creation': date.today(), 'data_last_modified': date.today()}
-        ]
-
         product_for_create = []
-        for product_item in product_list:
-            product_for_create.append(Product(**product_item))
-        now = timezone.now()
-        x = Product.objects.filter(date_of_creation=now)
-        if x:
-            x.delete()
-            Product.objects.bulk_create(product_for_create)
-        else:
-            Product.objects.bulk_create(product_for_create)
+        category_for_create = []
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        with open('data_category.json', 'r', encoding='utf-8') as f:
+            data_category = json.load(f)
+            for category in data_category:
+                id = category['pk']
+                title_category = category['fields']['title_category']
+                description_category = category['fields']['description_category']
+                category_for_create.append(Category(id, title_category, description_category))
+        with open('data_product.json', 'r', encoding='utf-8') as f:
+            data_products = json.load(f)
+            for product in data_products:
+                id = product['pk']
+                title_product = product['fields']['title_product']
+                description_product = product['fields']['description_product']
+                price = product['fields']['price']
+                category = product['fields']['category']
+                image = product['fields']['image']
+                date_of_creation = product['fields']['date_of_creation']
+                data_last_modified = product['fields']['data_last_modified']
+                product_for_create.append(
+                    Product(id=id, title_product=title_product, description_product=description_product,
+                            category=Category(category),
+                            image=image, price=price, date_of_creation=date_of_creation,
+                            data_last_modified=data_last_modified, ))
+        Category.objects.bulk_create(category_for_create)
+        Product.objects.bulk_create(product_for_create)
